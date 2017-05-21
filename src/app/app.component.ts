0a5e0c6c1b64d2e4bdbf0d3c31a9bb816fcbf076
Martin Ombura Jr <Martin Ombura Jr>
2017-05-21 10:09:41 +0300

    branching out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { AngularFireDatabase } from 'angularfire2/database';
-import { OnInit } from '@angular/core/core';
-import { GeolocationService } from './api/service/geolocation/geolocation.service';
 import { Component } from '@angular/core';
+import { GeolocationService } from './api/service/geolocation/geolocation.service';
+import { OnInit } from '@angular/core/core';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   constructor(private glS: GeolocationService, private afDB: AngularFireDatabase) {
-
+    this.glS.getLocation({}).map(location => this.glS.convertToObject(location))
+            .map(geolocation => {console.log(geolocation); this.glS.upload(geolocation); }).subscribe(x=>console.log(x))
+    this.glS.retrieveAll().subscribe(x=>console.log(x));
   }
 
   ngOnInit() {
-    let a = this.glS.getLocation({}).map(location => this.glS.convertToObject(location))
-            .map(geolocation => {console.log(geolocation); this.glS.upload(geolocation); }).subscribe(x=>console.log(x))
-    this.glS.retrieveAll().subscribe(x=>console.log(x));
-}
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { GeolocationRepository } from './api/repository/geolocation/geolocation.repository';
 import { GeolocationService } from './api/service/geolocation/geolocation.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -21,6 +22,7 @@ import { environment } from '../environments/environment';
     AngularFireModule.initializeApp(environment.firebase, 'martinomburajr-a1bcd'), // imports firebase/app needed for everything
     AngularFireDatabaseModule, // imports firebase/database, only needed for database features
     AngularFireAuthModule,
+    NgbModule.forRoot()
   ],
   providers: [GeolocationService, GeolocationRepository],
   bootstrap: [AppComponent]
